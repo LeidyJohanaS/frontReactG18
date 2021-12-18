ce95989d78431e4e0342faa089b3104c5f4b8a5e
Santiago Pérez <santiago.perez.mt@correo.usa.edu.co>
2021-12-17 19:25:29 -0500

    Arregladas tabulaciones y se agrega función para obtener solo la fecha

--- src/ciclo4/components/shared/Functions.js
+++ src/ciclo4/components/shared/Functions.js
@@ -1,31 +1,37 @@
-function validarFormatoCorreo(correo){
-    if (/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(correo)){
-        return true
-    }
+function validarFormatoCorreo(correo) {
+  if (
+    /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(correo)
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function validaesVacio(dato) {
+  return !dato.trim().length;
+}
+
+function isNumeric(numstr) {
+  if (numstr.match(/^\d+$/)) {
+    return true;
+  } else {
     return false;
-
+  }
 }
 
-function validaesVacio(dato){
-    return !dato.trim().length;
+function getJustDate(fullDate) {
+  var day = new Date(fullDate);
+  var dd = String(day.getDate()).padStart(2, "0");
+  var mm = String(day.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = day.getFullYear();
+  return  yyyy+"-" +mm + "-"+ dd;
 }
 
-function isNumeric(numstr)
-{
-    if (numstr.match(/^\d+$/ ) ) {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-
-const exportObj={
-    validarFormatoCorreo,
-    validaesVacio,
-    isNumeric
-}
+const exportObj = {
+  validarFormatoCorreo,
+  validaesVacio,
+  isNumeric,
+  getJustDate,
+};
 
 export default exportObj;
