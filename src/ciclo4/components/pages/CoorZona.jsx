05b271aa300f19b76018b3967a47e61ddd59bc72
Santiago Pérez <santiago.perez.mt@correo.usa.edu.co>
2021-12-18 17:29:26 -0500

    Agregada orden que revisa el coordinador de zona

--- src/ciclo4/components/pages/CoorZona.jsx
+++ src/ciclo4/components/pages/CoorZona.jsx
@@ -1,50 +1,139 @@
-import Form from 'react-bootstrap/Form'
+import Form from "react-bootstrap/Form";
 import React, { useState } from "react";
-import {
-  Button,
-  Modal,
-  Col,
-  Row,
-} from "react-bootstrap";
+import OrderService from "../../services/OrderService";
+import Functions from "../shared/Functions";
+import { Button, Modal, Col, Row } from "react-bootstrap";
 
-function CoorZona() {
+class CoorZona extends React.Component {
+  state = {
+    orders: [],
+  };
+  componentDidMount() {
+    this.getOrders();
+  }
+  getOrders= () =>{
+    OrderService.getAll()
+      .then((response) => {
+        this.setState({ orders: response.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  render() {
+    return (
+      <>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-10">
+              <h1>Orden</h1>
+            </div>
+          </div>
+          <table className="table table-bordered border-dark scroll-area">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Fecha del pedido</th>
+                <th>Estado</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.orders.map((order, index) => (
+                <tr key={index}>
+                  <td>{order.id}</td>
+                  <td>{Functions.getJustDate(order.registerDay)}</td>
+                  <td>{order.status}</td>
+                  <td>
+                    <ModalOrden
+                      order={order}
+                      getOrders={this.getOrders}
+                    ></ModalOrden>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
+}
+export default CoorZona;
+
+function ModalOrden(props) {
   const [show, setShow] = useState(false);
+  const [order, setOrder] = useState(props.order);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  
-  
+  const [status, setStatus] = useState(
+    props.order.status === "Aprobada" || props.order.status === "Pendiente"
+      ? "Aprobada"
+      : "Rechazada"
+  );
+  function changeStatus(e, newStatus) {
+    setStatus(newStatus);
+  }
+  function updateOrder() {
+    let copyOrder = order;
+    copyOrder.status = status;
+    setOrder(copyOrder);
+    OrderService.update(order)
+      .then((response) => {
+        alert("Orden Actualizada");
+        handleClose();
+        props.getOrders();
+      })
+      .catch((err) => {});
+  }
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-10">
-            <h1>Orden</h1>
-          </div>
-        </div>
-        <table className="table table-bordered border-dark scroll-area">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Fecha del pedido</th>
-            <th>Estado</th>
-            <th>Acciones</th>
-          </tr>
-          <Button variant="primary" onClick={handleShow}>
-                Detalle Orden
-              </Button>
-        </thead>
-      
-               
-      </table>
-        </div>
-      
-
-      <Modal show={show} onHide={handleClose}>
+      <Button variant="primary" onClick={handleShow}>
+        Ver detalle orden
+      </Button>
+      <Modal show={show} onHide={handleClose} size="lg">
         <Modal.Header closeButton>
           <Modal.Title>Detalle Pedido</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <h4>Orden Pedido</h4>
+          <table className="table table-bordered border-dark scroll-area">
+            <thead>
+              <tr>
+                <th>Referencia</th>
+                <th>Categoría</th>
+                <th>Descripción</th>
+                <th>Disponibilidad</th>
+                <th>Precio</th>
+                <th>Foto</th>
+                <th>Cantidad solicitada</th>
+                <th>Stock</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(props.order.products).map((key, index) => (
+                <tr key={index}>
+                  <td>{props.order.products[key].reference}</td>
+                  <td>{props.order.products[key].category}</td>
+                  <td>{props.order.products[key].description}</td>
+                  <td>
+                    {props.order.products[key].availability ? "Sí" : "No"}
+                  </td>
+                  <td>{props.order.products[key].price}</td>
+                  <td>
+                    <img
+                      src={props.order.products[key].photography}
+                      alt={key}
+                      width={100}
+                      height={50}
+                    />
+                  </td>
+                  <td>{props.order.quantities[key]}</td>
+                  <td>{props.order.products[key].quantity}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
           <fieldset>
             <Form.Group as={Row} className="mb-3">
               <Form.Label as="legend" column sm={2}>
@@ -56,12 +145,20 @@ function CoorZona() {
                   label="Aprobado"
                   name="formHorizontalOrden"
                   id="radioOrdenaprobada"
+                  defaultChecked={
+                    status === "Aprobada" || status === "Pendiente"
+                      ? true
+                      : false
+                  }
+                  onChange={(e) => changeStatus(e, "Aprobada")}
                 />
                 <Form.Check
                   type="radio"
                   label="Rechazado"
                   name="formHorizontalOrden"
                   id="radioOrdenrechazada"
+                  defaultChecked={status === "Rechazada" ? true : false}
+                  onChange={(e) => changeStatus(e, "Rechazada")}
                 />
               </Col>
             </Form.Group>
@@ -71,7 +168,7 @@ function CoorZona() {
           <Button variant="secondary" onClick={handleClose}>
             Cerrar
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={updateOrder}>
             Actualizar
           </Button>
         </Modal.Footer>
@@ -79,5 +176,3 @@ function CoorZona() {
     </>
   );
 }
-export default CoorZona;
-
